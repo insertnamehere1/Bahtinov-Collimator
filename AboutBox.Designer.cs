287654ae6745815d3ad2fa47516b4ff04e7ec26f
chris <chris@chrisandbev.com.au>
2023-06-05 17:12:34 +1000

    Changes to About to display correct version numbers for network deploy

--- AboutBox.Designer.cs
+++ AboutBox.Designer.cs
@@ -149,6 +149,7 @@
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 24;
             this.okButton.Text = "&OK";
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
             // AboutBox
             // 
